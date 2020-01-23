dcf590ea4d4dd38ba563c49abf25b00a5c14cff0
Sebastian Ohl <sebastian@ohl.name>
2020-01-23 20:05:09 +0100

    building and running on ECU

--- main/main.c
+++ main/main.c
@@ -1,16 +1,12 @@
-#include "driver/gpio.h"
+#include <string.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
 #include "freertos/task.h"
-#include "nvs_flash.h"
-#include "sdkconfig.h"
-#include <stdio.h>
-
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_spi_flash.h"
-#include "esp_system.h"
+#include "freertos/event_groups.h"
+#include "rom/ets_sys.h"
 #include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
 #include "mqtt_client.h"
 
@@ -30,37 +26,38 @@ void update_data(struct homie_handle_s *handle, int node, int property);
 homie_handle_t homie = {
     .deviceid = "beamer-control",
     .devicename = "Beamer Control",
-	.firmware = GIT_URL,
-	.firmware_version = GIT_BRANCH" "GIT_COMMIT_HASH,
-    .update_interval =
-        0, /* set to 0 to workaround openhab problem of taking device offline */
+    .update_interval = 0, /* set to 0 to workaround openhab problem of taking device offline */
     .num_nodes = 1,
-    .nodes =
-        {
-            {.id = "inflow-floor-heating",
-             .name = "Inflow Floor Heating",
-             .type = "ds18b20",
-             .num_properties = 1,
-             .properties =
-                 {
-                     {
-                         .id = "temperature",
-                         .name = "Temperature",
-                         .settable = HOMIE_FALSE,
-                         .retained = HOMIE_TRUE,
-                         .unit = "°C",
-                         .datatype = HOMIE_FLOAT,
-                         .read_property_cbk = &update_data,
-                     },
-                 }
-            }
-        },
+	//       .firmware = GIT_URL,
+	//       .firmware_version = GIT_BRANCH" "GIT_COMMIT_HASH,
+	    .update_interval = 0, /* set to 0 to workaround openhab problem of taking device offline */
+	    .num_nodes = 1,
+	    .nodes =
+	        {
+	            {.id = "inflow-floor-heating",
+	             .name = "Inflow Floor Heating",
+	             .type = "ds18b20",
+	             .num_properties = 1,
+	             .properties =
+	                 {
+	                     {
+	                         .id = "temperature",
+	                         .name = "Temperature",
+	                         .settable = HOMIE_FALSE,
+	                         .retained = HOMIE_TRUE,
+	                         .unit = "°C",
+	                         .datatype = HOMIE_FLOAT,
+	                         .read_property_cbk = &update_data,
+	                     },
+	                 }
+	            }
+	        },
     .uptime = 0,
 };
 
 int wifi_retry_count = 0;
 const int WIFI_CONNECTED_BIT = BIT0;
-static const char *TAG = "heating_controller";
+static const char *TAG = "beamer-control";
 
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
@@ -104,16 +101,39 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
     return ESP_OK;
 }
 
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data)
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
-    {
+    /* For accessing reason codes in case of disconnection */
+    system_event_info_t *info = &event->event_info;
+
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
         esp_wifi_connect();
-    }
-    else if (event_base == WIFI_EVENT &&
-             event_id == WIFI_EVENT_STA_DISCONNECTED)
-    {
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+        snprintf(homie.ip, sizeof(homie.ip), "%s",
+                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+        wifi_retry_count = 0;
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_AP_STACONNECTED:
+        ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d",
+                 MAC2STR(event->event_info.sta_connected.mac),
+                 event->event_info.sta_connected.aid);
+        break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+        ESP_LOGI(TAG, "station:"MACSTR"leave, AID=%d",
+                 MAC2STR(event->event_info.sta_disconnected.mac),
+                 event->event_info.sta_disconnected.aid);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        ESP_LOGE(TAG, "Disconnect reason : %d", info->disconnected.reason);
+        if (info->disconnected.reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT) {
+            /*Switch to 802.11 bgn mode */
+            esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCAL_11B | WIFI_PROTOCAL_11G | WIFI_PROTOCAL_11N);
+        }
         esp_wifi_connect();
         xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
         wifi_retry_count++;
@@ -124,34 +144,23 @@ static void event_handler(void *arg, esp_event_base_t event_base,
             fflush(stdout);
             esp_restart();
         }
+        break;
+    default:
+        break;
     }
-    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
-    {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        snprintf(homie.ip, sizeof(homie.ip), "%d.%d.%d.%d",
-                 IP2STR(&event->ip_info.ip));
-        wifi_retry_count = 0;
-        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
-    }
+    return ESP_OK;
 }
 
 void connect_to_wifi()
 {
     wifi_event_group = xEventGroupCreate();
 
-    esp_netif_init();
+    tcpip_adapter_init();
 
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL) );
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                               &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                                               &event_handler, NULL));
 
     wifi_config_t wifi_config = {
         .sta = {.ssid = CONFIG_WIFI_SSID, .password = CONFIG_WIFI_PASSWORD},
@@ -202,13 +211,12 @@ void update_data(struct homie_handle_s *handle, int node,
 void app_main(void)
 {
     printf("starting....\n");
-    // Initialize NVS
+
+    //Initialize NVS
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-        ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
